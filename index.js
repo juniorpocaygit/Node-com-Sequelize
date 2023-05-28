886db679c82497b4ad8cb9c267c3fde040fbd170
juniorpocaygit <juniorpocay@gmail.com>
2023-05-28 00:25:49 -0300

    finalizado CRUD com sequelize

--- index.js
+++ index.js
@@ -19,8 +19,89 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get ('/', (req,res) => {
-    res.render('home')
+app.get('/users/create', (req,res) => {
+    res.render('adduser')
+})
+
+app.post('/users/create', async (req,res) => {
+
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if (newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    await User.create({name, occupation, newsletter})
+
+    res.redirect ('/')
+
+})
+
+app.get ('/users/:id', async (req,res) => {
+
+    const id = req.params.id
+
+    const user = await User.findOne({raw: true, where: {id: id}})
+
+    res.render('userviews', { user })
+})
+
+app.post('/users/delete/:id', async (req,res) => {
+
+    const id = req.params.id
+
+    await User.destroy({where: {id: id}})
+
+    res.redirect('/')
+
+})
+
+app.get('/users/edit/:id', async (req,res) => {
+
+    const id = req.params.id
+
+    const user = await User.findOne({raw: true, where: {id: id}})
+
+    res.render('useredit', { user })
+
+})
+
+app.post ('/users/update', async (req,res) => {
+
+    const id = req.body.id
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === "on"){
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    const userData = {
+        id,
+        name,
+        occupation,
+        newsletter,
+    }
+
+    await User.update(userData, { where: {id: id}})
+
+    res.redirect('/')
+})
+
+
+
+app.get ('/', async (req,res) => {
+
+    const users = await User.findAll({raw: true})
+
+    res.render('home', { users: users})
 })
 
 conn.sync().then(() => {
